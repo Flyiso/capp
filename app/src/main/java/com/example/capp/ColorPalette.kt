84bf2b688397f85b9f5588843b22a5d1f194313b
Flyiso <dexterpetterssonspang@gmail.com>
2026-05-21 16:01:33 +0200

    typo updates

--- app/src/main/java/com/example/capp/ColorPalette.kt
+++ app/src/main/java/com/example/capp/ColorPalette.kt
@@ -501,7 +501,7 @@ data class ColorPalette(
                     "HEX" -> matchObj.hexColorStr
                     "HSV" -> matchObj.hsvColorStr
                     "CMY" -> matchObj.cmyColorStr
-                    "NCS" -> "${matchObj.ncsColorStr}\n${matchObj.matchPercentageNcs}% Match.)"
+                    "NCS" -> "${matchObj.ncsColorStr}\n(${matchObj.matchPercentageNcs}% Match)"
                     else -> matchObj.rgbColorStr
                 }
                 textViews[index].text = displayString
